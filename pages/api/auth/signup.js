7cb4460af57ef0e11a3607b25754901f0e7530e6
sheng yeong <chia.shengyeong@gmail.com>
2022-08-30 15:08:02 +0800

    sending sign up requests from front end

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,6 +2,11 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
+  
+  if (req.method !== "POST") {
+    return;
+  }
+  
   const data = req.body;
 
   const { email, password } = data;
@@ -20,7 +25,7 @@ async function handler(req, res) {
 
   const db = client.db();
 
-  const hashedPassword = hashPassword(oassword);
+  const hashedPassword = hashPassword(password);
 
   const result = await db.collection("users").insertOne({
     email: email,
