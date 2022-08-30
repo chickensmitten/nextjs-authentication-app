9c14552ba26abb110cd027cba588c721ec663fe9
sheng yeong <chia.shengyeong@gmail.com>
2022-08-30 14:34:33 +0800

    initialising storing of email and password with next-auth, bcryptjs and mongodb

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Next JS Authentication App
 
+Use Next-Auth library
+```
+npm install --save-exact next-auth@3
+npm install mongodb
+npm install bcryptjs
+```
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,33 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({ message: "Invalid Input" });
+    return;
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(oassword);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword
+  });
+
+  res.status(201).json({message: "Created user!"});
+}
+
+export default handler;
